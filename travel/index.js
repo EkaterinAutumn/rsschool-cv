66e7eef3db69103cef1194e37045d3f7470d6694
EkaterinAutumn <ek.erokhina@gmail.com>
2022-07-04 15:53:45 +0200

    Feat: add code for menu burger

--- travel/index.js
+++ travel/index.js
@@ -2,3 +2,19 @@ function assessment() {
     console.log('Вёрстка валидная +10 \nВёрстка семантическая +20 \nВёрстка соответствует макету +48 \nТребования к css +12 \nИнтерактивность, реализуемая через css +20 \n')
 }
 assessment()
+
+
+const menu = document.getElementById('menu');
+const menuItems = document.getElementsByClassName('link-header');
+const shadowBody = document.getElementById('shadow');
+function toggleMenu() {
+menu.classList.toggle('active')
+shadowBody.classList.toggle('active')
+}
+
+burger.onclick = toggleMenu;
+closeButton.onclick = toggleMenu;
+shadow.onclick = toggleMenu;
+for (let i=0; i < menuItems.length; i++) {
+    menuItems[i].onclick = toggleMenu;
+}
