a64bc630cf8962bd72924fdfe144df76bf720e3d
EkaterinAutumn <ek.erokhina@gmail.com>
2022-08-02 18:57:44 +0200

    Feat: add timer and greeting

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -1,4 +1,46 @@
 import './styles.css'
 
-const timeOfDay = document.getElementById('timeofday')
-console.log(timeOfDay)
+const time = document.querySelector('.time');
+const greeting = document.getElementById('greeting')
+const date = new Date();
+let currentTime = date.toLocaleTimeString();
+const day = document.querySelector('.date');
+
+function updateTime() {
+    const currentDate = new Date();
+
+    showGreeting(currentDate);
+
+    currentTime = currentDate.toLocaleTimeString();
+    showTime();
+    setTimeout(updateTime, 1000);
+}
+
+function showTime() {
+    time.textContent = new Date().toLocaleTimeString();
+}
+
+updateTime()
+
+
+function getTimeOfDay(hour) {
+    if (hour >= 6 && hour < 12) {
+        return 'morning';
+    } else if (hour >= 12 && hour < 18) {
+        return 'afternoon';
+    } else if (hour >= 18 && hour < 24) {
+        return 'evening';
+    } else if (hour >= 0 && hour < 6) {
+        return 'night';
+    }
+}
+
+function showGreeting(date) {
+    const hour = date.getHours();
+    const timeOfDay = getTimeOfDay(hour);
+    greeting.textContent = `Good ${timeOfDay}`
+}
+
+function showDay(day) {
+    
+}
