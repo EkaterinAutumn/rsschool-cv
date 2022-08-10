d43c776107d40059feed0f3c3c6365afde169c94
EkaterinAutumn <ek.erokhina@gmail.com>
2022-08-10 12:36:26 +0200

    Refactoring: modify weather

--- momentum/src/weather.module.js
+++ momentum/src/weather.module.js
@@ -6,20 +6,42 @@ export function fetchWeather() {
   const wind = document.querySelector('.wind');
   const humidity = document.querySelector('.humidity');
   const city = document.querySelector('.city');
+  const weatherContainer = document.querySelector('.weather-value');
+  const error = document.querySelector('.error');
 
   async function getWeather() {
     const geoCode = city.value;
-    if (geoCode) {const url = `https://api.openweathermap.org/data/2.5/weather?q=${geoCode}&lang=en&appid=7f8d87d7cf15aeb3d56bb0374f31bdb7&units=metric`;
-    const res = await fetch(url);
-    const data = await res.json();
-
-    weatherIcon.className = 'weather-icon owf';
-    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-    temperature.textContent = `${Math.round(data.main.temp)}°C`;
-    weatherDescription.textContent = data.weather[0].description;
-    wind.textContent = `${Math.round(data.wind.speed)} m/s`;
-    humidity.textContent = `${data.main.humidity}%`;
+    if (geoCode) {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${geoCode}&lang=en&appid=7f8d87d7cf15aeb3d56bb0374f31bdb7&units=metric`;
+      try {
+        const res = await fetch(url);
+        if (res) {
+          const data = await res.json();
+          weatherIcon.className = 'weather-icon owf';
+          weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+          temperature.textContent = `${Math.round(data.main.temp)}°C`;
+          weatherDescription.textContent = data.weather[0].description;
+          wind.textContent = `${Math.round(data.wind.speed)} m/s`;
+          humidity.textContent = `${data.main.humidity}%`;
+          hideError();
+        }
+      } catch (err) {
+        displayError({geocodeError: geoCode});
+      }
+    }
   }
+
+  function displayError(errorObj) {
+    weatherContainer.classList.add('error');
+    error.classList.remove('error');
+    error.classList.add('error-text');
+    error.textContent = `Error! City not found for "${errorObj.geocodeError}"!`;
+  }
+
+  function hideError() {
+    weatherContainer.classList.remove('error');
+    error.classList.add('error');
+    error.classList.remove('error-text');
   }
 
   function setCity(event) {
@@ -36,16 +58,16 @@ export function fetchWeather() {
   const input = document.getElementById('inputweather')
 
   function setCityInLocalStorage() {
-      localStorage.setItem('city', inputweather.value);
+    localStorage.setItem('city', inputweather.value);
   }
-  
+
   function getCityInLocalStorage() {
-      if (localStorage.getItem('city')) {
-        inputweather.value = localStorage.getItem('city');
-      } else {
-        inputweather.value = 'Moscow';
-      }
-      getWeather();
+    if (localStorage.getItem('city')) {
+      inputweather.value = localStorage.getItem('city');
+    } else {
+      inputweather.value = 'Moscow';
+    }
+    getWeather();
   }
   window.addEventListener('load', getCityInLocalStorage)
 
