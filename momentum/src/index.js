4417a5240339804d46f99f0fab8c128c8a900d30
EkaterinAutumn <ek.erokhina@gmail.com>
2022-08-16 13:16:17 +0200

    Refactoring: correct some classes

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -7,9 +7,13 @@ import {getQuotes} from './quotes.module.js';
 import {getMusic} from './pleer.module.js';
 import {getPhoto} from './slider.module.js';
 
+
+
 getPhoto();
 greeting();
 getTimeDay();
 fetchWeather();
 getQuotes();
 getMusic();
+
+console.log('Часы и календарь +15 \nПриветствие +10 \nСмена фонового изображения +20 \nВиджет погоды +15 \nВиджет цитата дня +10 \nАудиоплеер +15 \nИтого 85, остальные пункты не делала');
